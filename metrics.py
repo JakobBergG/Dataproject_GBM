7aa91eaacd871e47f21f7b12e36ea011106ca5d9
Peter Olsen <pjol@protonmail.com>
2023-02-23 10:12:48 +0100

    add function mask_overlap

--- metrics.py
+++ metrics.py
@@ -7,7 +7,14 @@ def volume_mask(image : sitk.Image) -> int:
     im_view = sitk.GetArrayViewFromImage(image)
     return np.count_nonzero(im_view)
 
+
 # return volume of mask in cubic centimetres
-def volume_mask_cc(image : sitk.Image) -> int:
+def volume_mask_cc(image : sitk.Image) -> float:
     spacing = image.GetSpacing()
     return spacing[0] * spacing[1] * spacing[2] * volume_mask(image) / 1000.0
+    
+
+# return percentage of overlap between gtv and (95%) dose
+def mask_overlap(gtv : sitk.Image, dose : sitk.Image) -> float:
+    return volume_mask(dose*gtv) / volume_mask(gtv)
+
