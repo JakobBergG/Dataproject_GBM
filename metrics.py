43a6cd43eb444a1ec84114f9dd0c1c86a8b2e494
Peter Olsen <pjol@protonmail.com>
2023-02-23 10:16:05 +0100

    add docstrings

--- metrics.py
+++ metrics.py
@@ -2,19 +2,19 @@ import SimpleITK as sitk
 import numpy as np
 
 
-# count the number of voxels in a mask
 def volume_mask(image : sitk.Image) -> int:
+    '''Count the number of voxels in a mask'''
     im_view = sitk.GetArrayViewFromImage(image)
     return np.count_nonzero(im_view)
 
 
-# return volume of mask in cubic centimetres
 def volume_mask_cc(image : sitk.Image) -> float:
+    '''Get volume of mask in cubic centimetres'''
     spacing = image.GetSpacing()
     return spacing[0] * spacing[1] * spacing[2] * volume_mask(image) / 1000.0
     
 
-# return percentage of overlap between gtv and (95%) dose
 def mask_overlap(gtv : sitk.Image, dose : sitk.Image) -> float:
+    '''Get percentage of overlap between gtv and (95%) dose'''
     return volume_mask(dose*gtv) / volume_mask(gtv)
 
