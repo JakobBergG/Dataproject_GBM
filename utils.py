73814ce3aa5d36895a9c354fecf4dc78a0a48690
Peter Olsen <pjol@protonmail.com>
2023-03-06 11:57:47 +0100

    add function to load journal info

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import SimpleITK as sitk
 from datetime import datetime
 import re
+import csv
 
 
 def reslice_image(itk_image : sitk.Image, itk_ref : sitk.Image , is_label : bool = False) -> sitk.Image:
@@ -26,6 +27,41 @@ def parse_filename(filename : str) -> tuple:
     return patient_id, date, scantype, datatype
 
 
-def date_to_relative_time(date, base_date) -> float:
+def date_to_relative_time(date : datetime, base_date : datetime) -> float:
     '''Returns date with relative time in days relative to base date'''
     return (date - base_date).total_seconds() / 86400
+
+
+def load_journal_info_patients(path : str) -> dict:
+    '''Loads .csv file with info about patient. Returns dictionary
+    with key for each patient'''
+
+    # these following columns should be read
+    to_read = {
+        "Study_ID",
+        "MRIDiagDate_checked",
+        "MRIPostopDate_checked",
+        "RT_MRIDate",
+        "Age_at_diagn",
+        "Sex"
+    }
+
+    # create dict to hold all patients
+    journal_info_patients = {}
+
+    with open(path, newline='', mode="r", encoding="utf-8-sig") as f:
+        rows = csv.reader(f, delimiter=";")
+        names = next(rows) # the first row gives the names of the columns
+        
+        # now read info for all patients
+        for row in rows:
+            # create dict for each patient
+            patient_dict = {}
+            for i, name in enumerate(names):
+                if name == "Study_ID":
+                    study_id = f"{row[i]:>04}" #pad with 4 zeros
+                elif name in to_read:
+                    patient_dict[name] = row[i]
+            journal_info_patients[study_id] = patient_dict
+    
+    return journal_info_patients
