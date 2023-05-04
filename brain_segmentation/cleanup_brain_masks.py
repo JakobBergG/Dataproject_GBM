af906a938b98e9f3caf7b109ab832c10f0dddf0d
Peter Olsen <pjol@protonmail.com>
2023-05-04 11:00:17 +0200

    remove todos and add some comments

--- brain_segmentation/cleanup_brain_masks.py
+++ brain_segmentation/cleanup_brain_masks.py
@@ -3,9 +3,6 @@ import common.utils as utils
 import SimpleITK as sitk
 import re
 
-# TODO: remove holes here
-# TODO: Get mask files directly
-
 basepath = utils.get_path("path_data")
 
 local_path_brainmasks_ct = utils.get_path("local_path_brainmasks_ct")
@@ -14,10 +11,13 @@ local_path_brainmasks_mr = utils.get_path("local_path_brainmasks_mr")
 def remove_small_object_and_save(mask_path: str, output_path: str):
     '''removes small object from mask image and saves new mask'''
     mask=sitk.ReadImage(mask_path)
+    # The connected component filters labels each connected bit of the mask
+    # 1, 2, 3, etc. respecitively
     connected_component = sitk.ConnectedComponentImageFilter()
     component_image = connected_component.Execute(mask)
-
+    # Relabel the components so that 1 is the largest, 2 is the next-largest etc
     label_component = sitk.RelabelComponentImageFilter()
+    # Only take the largest component
     label_image = label_component.Execute(component_image) == 1
     sitk.WriteImage(label_image, output_path)
     
@@ -30,7 +30,6 @@ def cleanup_brain_mask(patient_folder : str):
      path_mr = os.path.join(patient_folder,local_path_brainmasks_mr)  
      path_ct = os.path.join(patient_folder,local_path_brainmasks_ct)  
      
-
      # find all files in mask folder
      mr_filelist = [f.path for f in os.scandir(path_mr)]
      ct_filelist = [f.path for f in os.scandir(path_ct)]
@@ -42,7 +41,6 @@ def cleanup_brain_mask(patient_folder : str):
      for file in mr_filelist:
          if os.path.basename(file).endswith("_MR_res_mask.nii.gz"):
              mr_list.append(file)
-        
      for file in ct_filelist:
         if os.path.basename(file).endswith("_CT_res_mask.nii.gz"):
             ct = file
@@ -53,8 +51,6 @@ def cleanup_brain_mask(patient_folder : str):
         output_name = re.sub("_MR_res_mask", "_MR_res_mask_cleaned", mr)
         remove_small_object_and_save(mr, output_name)
         
-        
-
      # ct mask
      output_name = re.sub("_CT_res_mask", "_CT_res_mask_cleaned", ct)
      remove_small_object_and_save(ct, output_name)
