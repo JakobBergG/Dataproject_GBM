434d163bf7c51350ec08c8335fd9b182abee1541
bjarkekjar <bjarkekjar@gmail.com>
2023-05-11 12:58:44 +0200

    Fixed input

--- run_registration.py
+++ run_registration.py
@@ -5,8 +5,6 @@ from datetime import datetime
 import common.utils as utils
 import registration.registration_MR_mask_to_CT_mask_test
 import registration.mask_registration_evaluation
-import analysis.patient_metrics
-import analysis.patient_metrics_to_csv
 
 
 # setup of logging
@@ -33,14 +31,15 @@ def run_pipeline(patient_folder : str):
     #-------------------------#
     log.info(f"Starting registration for patient {patient_id}")
     try:
-        registration.registration_MR_mask_to_CT_mask_test.register_MR_to_CT(patient_folder)
+        registration.registration_MR_mask_to_CT_mask.register_MR_to_CT(patient_folder)
     except Exception as e:
         log.error(f"Registration failed for {patient_id}. Error message: {str(e)}")
         return
     
     # Evaluate registraion
+    registration_eval_filename = f"registration_mask_MSD_{date_str}.json"
     try:
-        registration.mask_registration_evaluation.add_msd_to_json(patient_folder)
+        registration.mask_registration_evaluation.add_msd_to_json(patient_folder, registration_eval_filename)
     except Exception as e:
         log.error(f"Registration evaluation failed for {patient_id}. Error message: {str(e)}")
         log.info("Continuing despite mask registration fail...")
@@ -71,8 +70,8 @@ def main():
         registration.mask_registration_evaluation.sort_msd_dict(registration_eval_filename)
     except Exception as e:
         log.error(f"MSD dictionary sort failed. Error message: {str(e)}")
-    # Convert patient_metrics json to csv
-    
+   
+
 
 
 if __name__ == "__main__":
