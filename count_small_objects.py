203194c04bf2410986cae2b4b427acf984f5c4c2
Peter Olsen <pjol@protonmail.com>
2023-05-23 13:11:58 +0200

    look at second biggest objects

--- count_small_objects.py
+++ count_small_objects.py
@@ -10,25 +10,34 @@ mr_files = [f.path for f in os.scandir(basepath_mr)]
 ct_scans_with_small_objects = 0
 mr_scans_with_small_objects = 0
 
-def n_small_objects(label_file : str) -> bool:
-    '''Returns true if the scan has small objects
+def size_second_largest(label_file : str, ignore_threshold = 5) -> bool:
+    '''Returns volume of second largest tumor is above threshold
     '''
     label_image = sitk.ReadImage(label_file)
     connected_component = sitk.ConnectedComponentImageFilter()
     connected_component.Execute(label_image)
-    return connected_component.GetObjectCount()
+    n = connected_component.GetObjectCount()
+    if n >= 2:
+        stats = sitk.LabelShapeStatisticsImageFilter()
+        stats.Execute(label_image)
+        component_sizes = sorted([stats.GetNumberOfPixels(l) for l in stats.GetLabels()], reverse=True)
+        if component_sizes[1] > ignore_threshold: # er den andenstørste for stor?
+            return component_sizes[1]
+        
+    return -1
 
 for label_image in ct_files:
-    n = n_small_objects(label_image)
-    if n >= 2:
-        print(f"{n} små objekter (CT) i {label_image}")
+    vol = size_second_largest(label_image)
+    if vol != -1:
+        print(f"Big ({vol} voxels) second largest component in {label_image}")
         ct_scans_with_small_objects +=1
 
 for label_image in mr_files:
-    n = n_small_objects(label_image)
-    if n >= 2:
-        print(f"{n} små objekter (MR) i {label_image}")
+    vol = size_second_largest(label_image)
+    if vol != -1:
+        print(f"Big ({vol} voxels) second largest component in {label_image}")
         mr_scans_with_small_objects +=1
+
 
 print(f"{ct_scans_with_small_objects =}")
 print(f"{mr_scans_with_small_objects =}")
