355e79aaf4e6668751f521fa9db3bfe69d055cf9
Peter Olsen <pjol@protonmail.com>
2023-05-31 14:03:43 +0200

    fix spelling in metrics comments

--- common/metrics.py
+++ common/metrics.py
@@ -32,17 +32,17 @@ def mask_overlap(image1 : sitk.Image, image2 : sitk.Image) -> float:
     if vol1 > 0:
         return volume_mask(image2*image1) / vol1
     else:
-        raise Exception("Volune of mask is 0")
+        raise Exception("Volume of mask is 0")
 
 
 def dose_percentage_region(dose_image : sitk.Image, target_intensity : float, percentage : float = 0.95) -> sitk.Image:
-    '''Create a mask of where the dose is above a certain percentage of the
+    '''Create a mask of where the RTDOSE is above a certain percentage of the
       target intensity (e.g. 95% of 60 Gy)'''
     return dose_image >= target_intensity * percentage
 
 
 def get_target_dose (image: sitk.Image) -> int:
-    '''Guess target dose from maximum value in dose image (54 or 60)'''
+    '''Guess target dose from maximum value in RTDOSE image (54 or 60)'''
     MinMax = sitk.MinimumMaximumImageFilter()
     MinMax.Execute(image)
     if MinMax.GetMaximum() > 60:
