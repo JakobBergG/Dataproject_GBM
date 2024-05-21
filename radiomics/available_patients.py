56670966b860c50c929b55017b7197842d453e4c
Alexander <alexcan@hotmail.dk>
2024-05-21 09:21:55 +0200

    Intermediate code

---
+++ radiomics/available_patients.py
@@ -0,0 +1,35 @@
+import csv
+import os
+import json
+
+JOURNAL_PATH = "D:/GBM/radiomic_results/overview.csv"
+
+# LOAD TUMOR CLASS #
+journal_info_patients = set()
+with open(JOURNAL_PATH, newline='', mode="r", encoding="utf-8-sig") as f:
+        rows = csv.reader(f, delimiter=",")
+        names = next(rows) # The first row gives the names of the columns
+        
+        # Now read info for all patients
+        for row in rows:
+            study_id = f"{row[1]:>04}" # Pad with 4 zeros
+            journal_info_patients.add(study_id)
+
+print("Number of scans Anouk has classified", len(journal_info_patients))
+
+
+hospitals = ["AUH", "OUH", "CUH"]
+datapath = "D:\\GBM\\uni_gtv_tr_data\\"
+
+available_patients_data = set()
+for hospital in hospitals:
+    available_patients_data.update(os.listdir(datapath + hospital))
+
+print("Number of available MR scans are:", len(available_patients_data))
+
+intersection = journal_info_patients.intersection(available_patients_data)
+
+print("Patients in common:", len(intersection))
+
+with open("D:\\GBM\\radiomic_results\\available_patients.json", "w") as f:
+    json.dump(list(intersection), f)
